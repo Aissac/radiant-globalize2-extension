b88744d871ef0e187cc8b42bf0989128049f6621
cristi <cristi.duma@aissac.ro>
2009-07-29 11:53:25 +0300

    validate_uniqueness_of slug scope locale

--- lib/globalize2/page_extensions.rb
+++ lib/globalize2/page_extensions.rb
@@ -2,6 +2,14 @@ module Globalize2
   module PageExtensions
     def self.included(base)
       base.reflections[:children].options[:order] = 'pages.virtual DESC'
+      base.validate.delete_if { |v| v.options[:scope] == :parent_id }
+      base.send(:validate, :unique_slug)
+    end
+    
+    def unique_slug
+      if self.class.find(:first, :joins => "INNER JOIN page_translations on page_translations.page_id = pages.id", :conditions => ["pages.parent_id = ? AND page_translations.slug = ? AND page_translations.locale = ?", parent_id, slug, self.class.locale])
+        errors.add('slug', "must be unique")
+      end
     end
   end
 end
